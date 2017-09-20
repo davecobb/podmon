20884567f9f3bd3106b15e8937de16c735f4d623
davecobb <dccobb@gmail.com>
2017-09-20 07:59:56 +0100

    Update ftp.py

--- ftp.py
+++ ftp.py
@@ -3,10 +3,10 @@ from pyftpdlib.handlers import FTPHandler
 from pyftpdlib.servers import FTPServer
 
 authorizer = DummyAuthorizer()
-authorizer.add_user("user", "12345", "/home/pi/ftp", perm="elradfmw")
+authorizer.add_user("username", "password", "/home/pi/ftp", perm="elradfmw")
 
 handler = FTPHandler
 handler.authorizer = authorizer
 
-server = FTPServer(("192.168.1.163", 2123), handler)
+server = FTPServer(("127.000.000.001", 2123), handler)
 server.serve_forever()
